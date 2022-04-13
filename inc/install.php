bc3bc945b5e07f94575220a2b288e7406bf9bef0
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-04-13 15:35:19 +0600

    Update

--- inc/install.php
+++ inc/install.php
@@ -13,4 +13,32 @@ class Install {
 
 	}
 
+	public function create_tables() {
+
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+	}
+
+	public function schema() {
+		global $wpdb;
+
+		$collate = $wpdb->has_cap( 'collation' ) ? $wpdb->get_charset_collate() : '';
+		$max_index_length = 191;
+// https://www.tutorialspoint.com/mysql/mysql-data-types.htm
+		$tables = "
+		CREATE TABLE {$wpdb->prefix}vm_messages (
+			id bigint(20) unsigned NOT NULL auto_increment,
+			session varchar(255) NOT NULL,
+			start_time datetime NOT NULL,
+			updated_time datetime NOT NULL,
+			name varchar(250) NOT NULL,
+			email varchar(100) NOT NULL,
+			messages longtext NOT NULL,
+			is_read bigint(20) NOT NULL default '0'
+		  ) $collate;
+		";
+
+		return $tables;
+	}
+
 }

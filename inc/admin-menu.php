f08556716c761553a4d40398b8dab3fca1dc872c
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-04-14 12:48:37 +0600

    Update

--- inc/admin-menu.php
+++ inc/admin-menu.php
@@ -15,34 +15,30 @@ class Admin_Menu {
 
     public static function admin_menu() {
 
-		$settings = VM_PATH. 'inc/admin-ui/settings.php';
-
         add_menu_page( __( 'Video Message', 'wpwaxvm' ), __( 'Video Message', 'wpwaxvm' ), 'manage_options', 'video-message', '', 'dashicons-format-chat', 77 );
 
 		add_submenu_page( 'video-message', __( 'All Messages', 'wpwaxvm' ), __( 'All Messages', 'wpwaxvm' ), 'manage_options', 'video-message', array( __CLASS__, 'all_messages' ) );
 
 		add_submenu_page( 'video-message', __( 'Forms', 'wpwaxvm' ), __( 'Forms', 'wpwaxvm' ), 'manage_options', 'vm-forms', array( __CLASS__, 'forms' ) );
 
-        add_submenu_page( 'video-message', __( 'Settings', 'wpwaxvm' ), __( 'Settings', 'wpwaxvm' ), 'manage_options', 'vm-settings', $settings );
+        add_submenu_page( 'video-message', __( 'Settings', 'wpwaxvm' ), __( 'Settings', 'wpwaxvm' ), 'manage_options', 'vm-settings', array( __CLASS__, 'settings' ) );
 
 		add_submenu_page( 'video-message', __( 'Integrations', 'wpwaxvm' ), __( 'Integrations', 'wpwaxvm' ), 'manage_options', 'vm-integrations', array( __CLASS__, 'integrations' ) );
     }
 
     public static function all_messages() {
-		echo 'ttt';
+		include VM_PATH. 'inc/admin-ui/all-messages.php';
     }
 
     public static function forms() {
-		echo 'ttt';
+		include VM_PATH. 'inc/admin-ui/forms.php';
     }
 
     public static function settings() {
-		echo 'ttt';
+		include VM_PATH. 'inc/admin-ui/settings.php';
     }
 
 	public static function integrations() {
-		echo 'ttt';
+		include VM_PATH. 'inc/admin-ui/integrations.php';
     }
-
-
 }

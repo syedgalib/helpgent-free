2960d9def19e16683e9398aa717f86aa2e8c13a2
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-04-20 15:30:43 +0600

    Update

--- src/apps/chatbox/components/welcome/Welcome.js
+++ src/apps/chatbox/components/welcome/Welcome.js
@@ -9,7 +9,24 @@ function Welcome() {
 		return null;
 	}
 
-	return <div className={classes.welcome}>Welcome Box</div>;
+	return (
+		<div className={classes.welcome}>
+			<div>
+				Welcome to wpWax, Leave your question below and we'll get back
+				to you asap.
+			</div>
+			<div>[Video Screen]</div>
+			<div>How would you like to chat?</div>
+			<div>
+				<button>Video</button>
+				<button>Screen Record</button>
+				<button>Voice</button>
+				<button>Text</button>
+			</div>
+			<div>You can practise before sending</div>
+			<div>Powered by wpWax</div>
+		</div>
+	);
 }
 
 export default Welcome;

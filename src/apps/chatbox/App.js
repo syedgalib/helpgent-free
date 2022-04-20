006e2db333722da9d3b73a813246b1ff06320a8e
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-04-20 12:32:13 +0600

    Update

--- src/apps/chatbox/App.js
+++ src/apps/chatbox/App.js
@@ -1,9 +1,11 @@
 import Avatar from "./components/avatar/Avatar";
+import Welcome from "./components/welcome/Welcome";
 
 function App() {
 	return (
 		<>
 			<Avatar />
+			<Welcome />
 		</>
 	);
 }
---
+++ src/apps/chatbox/components/welcome/Welcome.js
@@ -0,0 +1,12 @@
+import classes from "./Welcome.scss";
+
+function Welcome() {
+
+	return (
+		<div className={classes.welcome}>
+			Welcome Box
+		</div>
+	);
+}
+
+export default Welcome;

e3904cd155472ae07e58543df33acd0b4618b347
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-04-28 15:07:29 +0600

    Update

--- src/apps/chatbox/components/ChatScreen.js
+++ src/apps/chatbox/components/ChatScreen.js
@@ -1,11 +1,18 @@
 import { useSelector, useDispatch } from "react-redux";
 
+import Container from "Chatbox/components/ui/Container";
 import Welcome from "Chatbox/components/Welcome";
+import Video from "Chatbox/components/video/Index";
 
 function ChatScreen() {
 	const chatScreen = useSelector((state) => state.chatScreen);
 
-	return <div>{chatScreen == "welcome" && <Welcome />}</div>;
+	return (
+		<Container>
+			{chatScreen == "welcome" && <Welcome />}
+			{chatScreen == "video" && <Video />}
+		</Container>
+	);
 }
 
 export default ChatScreen;
--- src/apps/chatbox/components/Welcome.js
+++ src/apps/chatbox/components/Welcome.js
@@ -1,33 +1,34 @@
-import Container from "Chatbox/components/ui/Container";
+import { useDispatch } from "react-redux";
+
 import { chatBoxActions } from "Chatbox/store/chatbox-slice";
 
 function Welcome() {
+	const dispatch = useDispatch();
+
 	function btnHandler(type) {
-		dispatch(chatBoxActions.chatType(type));
+		dispatch(chatBoxActions.chatScreen(type));
 		dispatch(chatBoxActions.chatStep("1"));
 	}
 
 	return (
-		<Container>
+		<div>
 			<div>
-				<div>
-					Welcome to wpWax, Leave your question below and we'll get
-					back to you asap.
-				</div>
-				<div>[Video Screen]</div>
-				<div>How would you like to chat?</div>
-				<div>
-					<button onClick={() => btnHandler("video")}>Video</button>
-					<button onClick={() => btnHandler("screenRecord")}>
-						Screen Record
-					</button>
-					<button onClick={() => btnHandler("audio")}>Voice</button>
-					<button onClick={() => btnHandler("text")}>Text</button>
-				</div>
-				<div>You can practise before sending</div>
-				<div>Powered by wpWax</div>
+				Welcome to wpWax, Leave your question below and we'll get back
+				to you asap.
 			</div>
-		</Container>
+			<div>[Video Screen]</div>
+			<div>How would you like to chat?</div>
+			<div>
+				<button onClick={() => btnHandler("video")}>Video</button>
+				<button onClick={() => btnHandler("screenRecord")}>
+					Screen Record
+				</button>
+				<button onClick={() => btnHandler("audio")}>Voice</button>
+				<button onClick={() => btnHandler("text")}>Text</button>
+			</div>
+			<div>You can practise before sending</div>
+			<div>Powered by wpWax</div>
+		</div>
 	);
 }
 

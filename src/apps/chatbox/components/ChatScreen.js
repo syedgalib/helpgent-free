4e0ad010abc92d7dd75fa5f13d4a4aee123e1a77
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-04-28 15:32:08 +0600

    Update

--- src/apps/chatbox/components/ChatScreen.js
+++ src/apps/chatbox/components/ChatScreen.js
@@ -1,8 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Container from "Chatbox/components/ui/Container";
 import Welcome from "Chatbox/components/Welcome";
 import Video from "Chatbox/components/video/Index";
+import Audio from "Chatbox/components/audio/Index";
+import Text from "Chatbox/components/text/Index";
+import ScreenRecord from "Chatbox/components/screen-record/Index";
 
 function ChatScreen() {
 	const chatScreen = useSelector((state) => state.chatScreen);
@@ -11,6 +14,9 @@ function ChatScreen() {
 		<Container>
 			{chatScreen == "welcome" && <Welcome />}
 			{chatScreen == "video" && <Video />}
+			{chatScreen == "audio" && <Audio />}
+			{chatScreen == "text" && <Text />}
+			{chatScreen == "screenRecord" && <ScreenRecord />}
 		</Container>
 	);
 }

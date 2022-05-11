e9cd9057af52c1db0153f4c4d3fbe78f881bdc5b
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-05-11 13:20:43 +0600

    Update

--- plugin-base.php
+++ plugin-base.php
@@ -10,21 +10,25 @@
  * Domain Path: /languages
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-define( 'VM_VERSION',     1.0 );
+define( 'VM_VERSION', 1.0 );
 define( 'VM_PLUGIN_FILE', __FILE__ );
-define( 'VM_PATH',        trailingslashit( plugin_dir_path( __FILE__ ) ) );
-define( 'VM_URL',         trailingslashit( plugin_dir_url( __FILE__ ) ) );
+define( 'VM_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'VM_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
 
 final class wpWax_Video_Messagge {
 
 	protected static $instance = null;
 
-	public $loader = [];
+	public $loader = array();
 
 	public function __construct() {
 		add_action( 'init', array( $this, 'load_textdomain' ) );
+
+		// $this->register_autoload();
 
 		$this->includes();
 		$this->initialize();
@@ -32,15 +36,35 @@ final class wpWax_Video_Messagge {
 
 	public static function instance() {
 		if ( null == self::$instance ) {
-			self::$instance = new self;
+			self::$instance = new self();
 		}
 
 		return self::$instance;
 	}
 
-    public function load_textdomain() {
-        load_plugin_textdomain( 'wpwaxvm', false, dirname( plugin_basename(__FILE__) ) . '/languages/' );
-    }
+	public function load_textdomain() {
+		load_plugin_textdomain( 'wpwaxvm', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	public function register_autoload() {
+		spl_autoload_register(
+			function ( $class_name ) {
+
+
+				if ( !str_starts_with( $class_name, 'wpWax\vm' ) ) {
+					return;
+				}
+
+				$file = strtolower( $class_name );
+				$file = str_replace( '_', '-', $file );
+				$file = explode( '\\', $file );
+				$file = array_pop( $file );
+				$file = $file . '.php';
+				$path = VM_PATH . 'inc/' . $file;
+				require_once $path;
+			}
+		);
+	}
 
 	public function includes() {
 		$this->autoload( 'inc' );
@@ -57,16 +81,19 @@ final class wpWax_Video_Messagge {
 	public function autoload( $dir ) {
 		$path = VM_PATH . $dir . '/';
 		foreach ( scandir( $path ) as $file ) {
-			if ( preg_match( "/.php$/i", $file ) ) {
-				require_once( $path . $file );
+			if ( preg_match( '/.php$/i', $file ) ) {
+				require_once $path . $file;
 			}
 		}
 	}
 
 	public function temp() {
-		add_action('wp_body_open', function() {
-			echo '<div id="root"></div>';
-		});
+		add_action(
+			'wp_body_open',
+			function() {
+				echo '<div id="root"></div>';
+			}
+		);
 	}
 
 }

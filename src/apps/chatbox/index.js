dfd6ce27c9cde98c2cc322fa24a518753a7bd6af
Md Kowsar Hossain <kowsar9989@gmail.com>
2022-05-11 16:06:54 +0600

    Update

--- src/apps/chatbox/index.js
+++ src/apps/chatbox/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
-import store from 'Chatbox/store/index';
+import { Provider } from "react-redux";
+import store from "Chatbox/store/index";
 import App from "Chatbox/App";
 
 document.addEventListener("DOMContentLoaded", function () {
-	window.store = store; // for debugging, remove later
-	ReactDOM.render(
-		<Provider store={store}>
-			<App />
-		</Provider>,
-		document.getElementById("wpwax-vm-chatbox")
-	);
+	const target = document.getElementById("wpwax-vm-chatbox");
+
+	if (target) {
+		ReactDOM.render(
+			<Provider store={store}>
+				<App />
+			</Provider>,
+			target
+		);
+	}
 });

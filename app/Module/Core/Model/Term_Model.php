fe8a355e2304647ac64d17a141a3f4b8bf8de991
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2022-06-19 09:29:36 +0600

    improved

--- app/Module/Core/Model/Term_Model.php
+++ app/Module/Core/Model/Term_Model.php
@@ -2,6 +2,7 @@
 
 namespace WPWaxCustomerSupportApp\Module\Core\Model;
 
+use \WP_Error;
 use WPWaxCustomerSupportApp\Model\DB_Model;
 
 class Term_Model extends DB_Model {
@@ -17,9 +18,30 @@ class Term_Model extends DB_Model {
      * Get Items
      * 
      * @param array $args
-     * @return array|null
+     * @return array
      */
     public static function get_items( $args = [] ) {
+        global $wpdb;
+
+		$table = self::get_table_name( self::$table );
+
+        $default = [];
+
+        $default['limit'] = 20;
+        $default['page']  = 1;
+
+        $args = ( is_array( $args ) ) ? array_merge( $default, $args ) : $default;
+
+		$limit  = $args['limit'];
+		$offset = ( $limit * $args['page'] ) - $limit;
+
+		$where = ' WHERE 1=1';
+
+		$select = "SELECT * FROM $table";
+
+		$query = $select . $where . " LIMIT $limit OFFSET $offset";
+
+		return $wpdb->get_results( $query, ARRAY_A );
 
     }
 
@@ -27,30 +49,93 @@ class Term_Model extends DB_Model {
      * Get Item
      * 
      * @param int $id
-     * @return array|null
+     * @return array|WP_Error
      */
     public static function get_item( $id ) {
-        
+        global $wpdb;
+
+        if ( empty( $id ) ) {
+            $message = __( 'The resource ID is required.', 'wpwax-customer-support-app' );
+            return new WP_Error( 403, $message );
+        }
+
+		$table = self::get_table_name( self::$table );
+
+		$query = $wpdb->prepare( "SELECT * FROM $table WHERE term_id = %d", array( $id ) );
+
+		$result = $wpdb->get_row( $query, ARRAY_A );
+
+        if ( empty( $result ) ) {
+            $message = __( 'Could not find the resource.', 'wpwax-customer-support-app' );
+            return new WP_Error( 403, $message );
+        }
+
+		return $result;
     }
 
     /**
      * Create Item
      * 
      * @param array $args
-     * @return array|null
+     * @return array|WP_Error
      */
     public static function create_item( $args = [] ) {
-        
+        global $wpdb;
+
+		$table = self::get_table_name( self::$table );
+
+        $default = [];
+
+        $args = ( is_array( $args ) ) ? array_merge( $default, $args ) : $default;
+
+        if ( ! isset( $args['term_id'] ) ) {
+            unset( $args['term_id'] );
+        }
+
+		$result = $wpdb->insert( $table, $args );
+
+        if ( ! $result ) {
+            $message = __( 'Could not create the resource.', 'wpwax-customer-support-app' );
+            return new WP_Error( 403, $message );
+        }
+
+        return self::get_item( $wpdb->insert_id );
     }
 
     /**
      * Update Item
      * 
      * @param array $args
-     * @return array|null
+     * @return array|WP_Error
      */
     public static function update_item( $args = [] ) {
+        global $wpdb;
         
+        if ( empty( $id ) ) {
+            $message = __( 'The resource ID is required.', 'wpwax-customer-support-app' );
+            return new WP_Error( 403, $message );
+        }
+
+		$table    = self::get_table_name( self::$table );
+		$old_data = self::get_item( $args['id'] );
+
+        if ( empty( $old_data ) ) {
+            $message = __( 'The resource not found.', 'wpwax-customer-support-app' );
+            return new WP_Error( 403, $message );
+        }
+
+        $args = ( is_array( $args ) ) ? array_merge( $old_data, $args ) : $old_data;
+
+        $where = ['term_id' => $args['id'] ];
+
+        $result = $wpdb->update( $table, $args, $where, null, '%d' );
+
+        if ( ! $result ) {
+            $message = __( 'Could not update the resource.', 'wpwax-customer-support-app' );
+            return new WP_Error( 403, $message );
+        }
+
+        return self::get_item( $args['id'] );
     }
 
     /**
@@ -60,7 +145,24 @@ class Term_Model extends DB_Model {
      * @return bool
      */
     public static function delete_item( $id ) {
-        
+        global $wpdb;
+
+        if ( empty( $id ) ) {
+            $message = __( 'The resource ID is required.', 'wpwax-customer-support-app' );
+            return new WP_Error( 403, $message );
+        }
+
+		$table = self::get_table_name( self::$table );
+		$where = ['term_id' => $id ];
+
+		$status = $wpdb->delete( $table, $where, '%d' );
+
+        if ( empty( $status ) ) {
+            $message = __( 'Could not delete the resource.', 'wpwax-customer-support-app' );
+            return new WP_Error( 403, $message );
+        }
+
+        return ( ! empty( $status ) ) ? true : false;
     }
 
 }

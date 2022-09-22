82ba2aef9b9b65f245286480c626f03721c75cfa
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2022-09-22 07:19:19 +0600

    improved

--- app/Module/Core/Asset/Global_Asset.php
+++ app/Module/Core/Asset/Global_Asset.php
@@ -55,8 +55,8 @@ class Global_Asset extends Enqueuer {
             'ver'  => $this->script_version,
         ];
 
-        $scripts['wpwax-customer-support-app-core-fonts-inter'] = [
-            'link' => 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap',
+        $scripts['wpwax-customer-support-app-core-google-fonts'] = [
+            'link' => 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
             'ver'  => $this->script_version,
         ];
 

106c906f8308c686e904ec94dd8395b31bdae92f
vairafiq <vairafiq@gmail.com>
2022-10-19 15:08:27 +0600

    add html email

--- app/Module/Messenger/Email/Message_Notification_Emails.php
+++ app/Module/Messenger/Email/Message_Notification_Emails.php
@@ -102,6 +102,7 @@ class Message_Notification_Emails {
         $subject = ( ! empty( $args['subject'] ) ) ? $args['subject'] : __( 'A new conversation has starterd', 'wpwax-customer-support-app' );
 
         $message = self::get_email_body( $template_data );
+        $message = self::email_html( $subject, $message );
         $headers = self::get_email_headers( $template_data );
 
         return self::send_email( $to, $subject, $message, $headers );
@@ -215,6 +216,119 @@ class Message_Notification_Emails {
      */
     public static function mail_content_type() {
         return 'text/html';
+    }
+
+    /**
+     * Get Mail HTML
+     * 
+     * @param string $subject
+     * @param string $message
+     * @return string Email row html
+     */
+    public static function email_html($subject, $message){
+        $site_name = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
+        $header = '';
+        $email_header_color = get_directorist_option('email_header_color', '#8569fb');
+        $allow_email_header = get_directorist_option('allow_email_header', 1);
+        $author = "<a href='https://wpwax.com/'>wpWax</a>";
+        if ($allow_email_header){
+            $header = apply_filters('atbdp_email_header', '<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header" style=\'background-color: '.$email_header_color.'; color: #ffffff; border-bottom: 0; font-weight: bold; line-height: 100%; vertical-align: middle; font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif; border-radius: 3px 3px 0 0;\'>
+                                            <tr>
+                                                <td id="header_wrapper" style="padding: 36px 48px; display: block;">
+                                                    <h1 style=\'font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif; font-size: 30px; font-weight: 300; line-height: 150%; margin: 0; text-align: left; text-shadow: 0 1px 0 #ab79a1; color: #ffffff;\'>'.$subject.'</h1>
+                                                </td>
+                                            </tr>
+                                        </table>');
+        }
+    
+        return '<!DOCTYPE html>
+    <html lang="en-US">
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>Directorist</title>
+        </head>
+        <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="padding: 0;">
+            <div id="wrapper" dir="ltr" style="background-color: #f7f7f7; margin: 0; padding: 70px 0; width: 100%; -webkit-text-size-adjust: none;">
+                <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+                    <tr>
+                        <td align="center" valign="top">
+                            <div id="template_header_image">
+                                                        </div>
+                            <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container" style="background-color: #ffffff; border: 1px solid #dedede; box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1); border-radius: 3px;">
+                                <tr>
+                                    <td align="center" valign="top">
+                                        <!-- Header -->
+                                        '.$header.'
+                                        <!-- End Header -->
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" valign="top">
+                                        <!-- Body -->
+                                        <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
+                                            <tr>
+                                                <td valign="top" id="body_content" style="background-color: #ffffff;">
+                                                    <!-- Content -->
+                                                    <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                                        <tr>
+                                                            <td valign="top" style="padding: 48px 48px 32px;">
+                                                                <div id="body_content_inner" style=\'color: #636363; font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif; font-size: 14px; line-height: 150%; text-align: left;\'>
+    '.$message.'
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                    <!-- End Content -->
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        <!-- End Body -->
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" valign="top">
+                            <!-- Footer -->
+                            <table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
+                                <tr>
+                                    <td valign="top" style="padding: 0; border-radius: 6px;">
+                                        <table border="0" cellpadding="10" cellspacing="0" width="100%">
+                                            <tr>
+                                                <td colspan="2" valign="middle" id="credit" style=\'border-radius: 6px; border: 0; color: #8a8a8a; font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif; font-size: 12px; line-height: 150%; text-align: center; padding: 24px 0;\'>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                            <!-- End Footer -->
+                        </td>
+                    </tr>
+                    <tr>
+					<td align="center" valign="top">
+						<!-- Footer -->
+						<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
+							<tr>
+								<td valign="top">
+									<table border="0" cellpadding="10" cellspacing="0" width="100%">
+										<tr>
+											<td colspan="2" valign="middle" id="credit">
+												' . sprintf( wp_kses_post( wpautop( wptexturize( apply_filters( 'wpwax_customer_support_app_email_footer_text', 'Built with ❤️ by %s' ) ) ) ), $author ) . '
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+						<!-- End Footer -->
+					</td>
+				</tr>
+                </table>
+            </div>
+        </body>
+    </html>';
     }
 
 }

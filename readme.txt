cd183514c923f58cdc3564f60e664860bc709fa7
vairafiq <vairafiq@gmail.com>
2022-11-23 15:41:17 +0600

    add missing files'

---
+++ readme.txt
@@ -0,0 +1,16 @@
+﻿=== HelpGent - A Next Generation Form ===
+Contributors: wpwax
+Donate link: https://helpgent.com/
+Tags: modern form, video support, form builder, contact, help
+Requires at least: 4.6
+Tested up to: 6.1
+Requires PHP: 7.4
+Stable tag: 0.1.0
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+A Next Generation Form to engage your website visitors nicely
+
+== Description ==
+
+Will be update soon
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpwax/helpgent',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8398eb391f3a0c5d42ebc7eb2b2c1a77e5572ebe',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpwax/helpgent' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8398eb391f3a0c5d42ebc7eb2b2c1a77e5572ebe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
